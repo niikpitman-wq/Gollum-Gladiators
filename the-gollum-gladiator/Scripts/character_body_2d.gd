528cdbc7ba71dea3d9daa1fc9ec6fd128d72a302
QristionMark <niik.pitman@cathcollege.school.nz>
2026-06-10 11:59:30 +1200

    run jump and idel animation was added

---
+++ the-gollum-gladiator/Scripts/character_body_2d.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@onready var animation : AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if direction < 0.1:
+			animation.flip_h = true
+	else:
+		animation.flip_h = false
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	if is_on_floor():
+		
+		if velocity.x > 0.1:
+				animation.play("run")
+		else:
+				animation.play("idel")
+	
+	else:
+		animation.play("jump")
+	
